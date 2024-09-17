97b3041e43ea17690ee81e2c45a2a94dd9463ac2
Bhasakr Roy <i.bhskr@hotmail.com>
2024-09-17 14:30:00 +0530

    fixed bugs

--- backend/index.js
+++ backend/index.js
@@ -8,14 +8,15 @@ import cors from "cors";
 import "dotenv/config";
 
 const app = express();
-// const corsOptions = {
-//   origin: "https://x-clone-7ppu.onrender.com",
-//   // origin: "http://localhost:5173", // Replace with your front-end URL
-//   credentials: true, // This is needed for sending cookies
-// };
 
-// app.use(cors(corsOptions));
-app.use(cors());
+// Configure CORS options
+const corsOptions = {
+  origin: "https://x-clone-7ppu.onrender.com",
+  credentials: true, // Allow credentials like cookies
+};
+
+// Apply CORS middleware
+app.use(cors(corsOptions));
 
 // Load environment variables
 // if (process.env.NODE_ENV !== 'production') {
