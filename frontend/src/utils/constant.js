3f5d6d9d919cbfe416877ffe0e20acdca3fcbd1c
Bhasakr Roy <i.bhskr@hotmail.com>
2024-09-17 13:58:33 +0530

    fixed bugs

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,6 +1,6 @@
-export const USER_API_END_POINT = "http://localhost:8080/api/v1/user";
-export const TWEET_API_END_POINT = "http://localhost:8080/api/v1/tweet";
+// export const USER_API_END_POINT = "http://localhost:8080/api/v1/user";
+// export const TWEET_API_END_POINT = "http://localhost:8080/api/v1/tweet";
 
 
-// export const USER_API_END_POINT = "https://x-clone-d0qt.onrender.com/api/v1/user";
-// export const TWEET_API_END_POINT = "https://x-clone-d0qt.onrender.com/api/v1/tweet";
+export const USER_API_END_POINT = "https://x-clone-7ppu.onrender.com/api/v1/user";
+export const TWEET_API_END_POINT = "https://x-clone-7ppu.onrender.com/api/v1/tweet";
